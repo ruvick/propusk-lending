21ce124d3cd1167ce2272a6f3792507119366286
Ruvick <rudikov.web@ya.ru>
2021-11-24 16:20:23 +0300

    Отключил лишнее

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -2,6 +2,7 @@
 // document.addEventListener('DOMContentLoaded', () => {
 // });
 
+
 const iconMenu = document.querySelector(".icon-menu");
 const body = document.querySelector("body");
 const menuBody = document.querySelector(".mob-menu");
@@ -27,12 +28,6 @@ if (menuListItemElems) {
   });
 }
 
-// // Строка поиска на мобилках 
-// if (mobsearch) {
-//   mobsearch.addEventListener("click", function () {
-//     headsearch.classList.toggle("_active");
-//   });
-// }
 
 // Закрытие моб меню при клике вне области меню 
 window.addEventListener('click', e => { // при клике в любом месте окна браузера
@@ -62,60 +57,4 @@ smotScrollElems.forEach(link => {
   })
 });
 
-
-// Полоса прокрутки в шапке
-// const scrollProgress = document.getElementById('scroll-progress');
-// const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-// window.addEventListener('scroll', () => {
-//   const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-//   scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
-// });
-
-
-// Ползунок выбора цены
-// const priceEl = document.querySelector(".price");
-
-// function changePrice(price) {
-//   priceEl.innerText = price;
-//   console.log(price);
-// };
-
-
-// // Подсказки
-// tippy('._tippy', {
-//   content: "Подсказка",
-// });
-
-
-// Поочередное открытие нескольких блоков меню, табы, либо что то еще
-// const BarIconElems = document.querySelectorAll('.sidebar__menu-open');
-// const BarLinkIconElems = document.querySelectorAll('.sidebar__menu-icon');
-// const BarSubMenuElems = document.querySelectorAll('.sidebar__submenu');
-
-// BarIconElems.forEach((btn, index) => {
-//   btn.addEventListener('click', () => {
-
-//     if (!btn.classList.contains('sidebar__menu-icon_active')) {
-
-//       BarSubMenuElems.forEach((BarSubMenuElem) => {
-//         BarSubMenuElem.classList.remove('active')
-//       });
-//       BarIconElems.forEach((BarIconElem) => {
-//         BarIconElem.classList.remove('sidebar__menu-icon_active')
-//       });
-//       BarLinkIconElems.forEach((BarLinkIconElem) => {
-//         BarLinkIconElem.classList.remove('sidebar__menu-icon_active')
-//       });
-
-//       BarSubMenuElems[index].classList.add('active')
-//       BarLinkIconElems[index].classList.add('sidebar__menu-icon_active')
-//       btn.classList.add('sidebar__menu-icon_active')
-//     } else {
-//       BarSubMenuElems[index].classList.remove('active')
-//       BarLinkIconElems[index].classList.remove('sidebar__menu-icon_active')
-//       btn.classList.remove('sidebar__menu-icon_active')
-//     }
-//   })
-// })
 
