04a2d3faf23d8d3157fa26a4fecfbf06faccbeb8
asmi046 <asmi046@gmail.com>
2022-01-27 09:31:58 +0300

    Merge branch 'main' of https://github.com/ruvick/propusk-lending into main

--- #src/sender.php
+++ #src/sender.php
@@ -5,6 +5,36 @@ header("Access-Control-Allow-Origin: *");
 // REQUEST_METHOD
 // print_r(json_encode($_SERVER));
 
+define('TELEGRAM_TOKEN_TO', '5174947430:AAGRwIsyVi3mfoAWZ_H0JQkVpPzD6SC4SFw');
+
+function message_to_telegram($text)
+{
+	$arr_chat = "381762556,57815731";
+	if($arr_chat) {
+		$arr_chat = explode(",",$arr_chat);
+	    $ch = curl_init();
+		
+		$chatSend = TELEGRAM_TOKEN_TO;
+		
+		for ($i = 0; $i<count($arr_chat); $i++) {
+		    curl_setopt_array(
+		        $ch,
+		        array(
+		            CURLOPT_URL => 'https://api.telegram.org/bot' . $chatSend . '/sendMessage',
+		            CURLOPT_POST => TRUE,
+		            CURLOPT_RETURNTRANSFER => TRUE,
+		            CURLOPT_TIMEOUT => 10,
+		            CURLOPT_POSTFIELDS => array(
+		                'chat_id' => trim($arr_chat[$i]),
+		                'text' => $text,
+		            ),
+		        )
+		    );
+		    $output = curl_exec($ch);
+		}
+	}
+}
+
 if (
     ($_SERVER["HTTP_ORIGIN"] !== "http://localhost:3000")&&
     ($_SERVER["HTTP_ORIGIN"] !== "https://propuskmkad-pro.ru") 
@@ -30,8 +60,17 @@ if (
                         <p>Форма: '.$_REQUEST['msg'].'</p>                   
                     </body>
                 </html>'; 
+
+                $messageBot = $_REQUEST["msg"].
+                        "\nИмя: ".$_REQUEST['name'].
+                        "\nТелефон: ".$_REQUEST['tel'].
+                        "\nE-mail: ".$_REQUEST['mail'].
+                        "\nГосномер: ".$_REQUEST['number'].
+                        "\nСообщение: ".$_REQUEST['msg']; 
+
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
         $headers .= "From: Заявка с сайта Пропуска МКАД <noreply@propuskmkad-pro.ru>\r\n";
+        message_to_telegram($messageBot);
         if (mail($to, $subject, $message, $headers)) {
             http_response_code(200);
             die(array());
